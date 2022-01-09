9b347d30d336f26bdadc5033f7fefee3f673013e
njkwon <njkwon@gmail.com>
2022-01-09 10:23:06 +0900

    6주차 실습 추가

--- shell/redshift-driver.sh
+++ shell/redshift-driver.sh
@@ -1,4 +1,4 @@
 #bin/bash
 
-wget https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.37.1061/RedshiftJDBC42-no-awssdk-1.2.37.1061.jar
-sudo mv RedshiftJDBC42-no-awssdk-1.2.37.1061.jar /usr/lib/spark/jars/
+wget https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/2.0.0.4/redshift-jdbc42-2.0.0.4.jar
+sudo mv redshift-jdbc42-2.0.0.4.jar /usr/lib/spark/jars/
